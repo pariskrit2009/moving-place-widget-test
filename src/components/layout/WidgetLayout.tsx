9e3c1a9107ebfa9b9a7fff7136bdb30174309280
Pariskrit <pariskrit.moktan@yipl.com.np>
2026-05-13 17:14:14 +0545

    fix:iframe height to 640px

--- src/components/layout/WidgetLayout.tsx
+++ src/components/layout/WidgetLayout.tsx
@@ -20,10 +20,10 @@ export function WidgetLayout({
 
   return (
     <div id="widget-content" className="w-full bg-gray-100">
-      <div className="h-[794px] overflow-auto relative w-full border border-[#b1bbc8] bg-white p-6 flex flex-col">
+      <div className="h-[640px] overflow-auto relative w-full border border-[#b1bbc8] bg-white px-6 flex flex-col">
         <div className="space-y-1"></div>
 
-        <div className="sticky top-0 flex justify-between gap-3 w-full mb-2">
+        <div className="sticky top-0 flex justify-between gap-3 w-full my-4 p-2 bg-white z-20">
           {navigateBack && (
             <Button
               variant="outline"
